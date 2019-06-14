03e4edeed8a230e49b0daccc66798a5ff99d60a3
marcusique <markajtay@gmail.com>
2019-06-14 11:30:56 +0200

    fixes

--- config/dev.js
+++ config/dev.js
@@ -1,3 +1,4 @@
 module.exports = {
-  telegramBotToken: '804046451:AAGa8ccrTGu6YWcF3ajucXBTUr4nWcWMxsY'
+  telegramBotToken: '804046451:AAGa8ccrTGu6YWcF3ajucXBTUr4nWcWMxsY',
+  apiUrl: 'https://ergast.com/api/f1/'
 };
--- index.js
+++ index.js
@@ -1,31 +1,48 @@
 const Telegraf = require('telegraf'),
   keys = require('./config/keys'),
+  apiUrl = keys.apiUrl,
   bot = new Telegraf(keys.telegramBotToken),
-  axios = require('axios'),
-  telegrafInlineMenu = require('telegraf-inline-menu');
+  axios = require('axios');
 /* Welcome Message */
 bot.start(ctx => ctx.reply('Welcome to F1 Stats Bot!'));
 
+/* Get Driver Standings by a Given Year [START]*/
 bot.command('/getdriverstandings', ctx => {
   ctx.reply('Enter a year: ');
   bot.on('text', ctx => {
-    axios
-      .get(`https://ergast.com/api/f1/${ctx.message.text}/driverStandings.json`)
-      .then(function(res) {
-        const data =
-          res.data.MRData.StandingsTable.StandingsLists[0].DriverStandings;
-        let reply = [];
-        for (let i = 0; i < data.length; i++) {
-          reply.push(
-            data[i].Driver.givenName +
-              ' ' +
-              data[i].Driver.familyName +
-              ` (${data[i].points})`
-          );
-        }
-        ctx.reply(reply.join('\n'));
-      });
+    if (
+      ctx.message.text >= 1950 &&
+      ctx.message.text <= new Date().getFullYear()
+    ) {
+      axios
+        .get(`${apiUrl}${ctx.message.text}/driverStandings.json`)
+        .then(function(res) {
+          const driverStandings =
+            res.data.MRData.StandingsTable.StandingsLists[0].DriverStandings;
+          let preparedReply = [];
+          for (let i = 0; i < driverStandings.length; i++) {
+            preparedReply.push(
+              i +
+                1 +
+                '. ' +
+                driverStandings[i].Driver.givenName +
+                ' ' +
+                driverStandings[i].Driver.familyName +
+                ` (${driverStandings[i].points})`
+            );
+          }
+          ctx.reply(preparedReply.join('\n'));
+        })
+        .catch(function(err) {
+          console.log(err);
+        });
+    } else {
+      ctx.reply(
+        'Please enter a year between 1950 and ' + new Date().getFullYear() + '.'
+      );
+    }
   });
 });
+/* Get Driver Standings by a Given Year [END]*/
 
 bot.launch();
